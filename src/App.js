8d9026cdf8878cd5ad66dedff4ee20542e0b2deb
Akash Srivastava <sri.akash1992@gmail.com>
2021-04-04 00:23:04 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { StreamChat } from "stream-chat";
+import {
+  Chat,
+  Channel,
+  ChannelHeader,
+  ChannelList,
+  MessageList,
+  MessageInput,
+  Thread,
+  Window,
+} from "stream-chat-react";
+import "stream-chat-react/dist/css/index.css";
 
-function App() {
+const filters = { type: "messaging" };
+const options = { state: true, presence: true, limit: 10 };
+const sort = { last_message_at: -1 };
+
+const client = StreamChat.getInstance("fgdbgjzw6abd");
+
+const App = () => {
+  const [clientReady, setClientReady] = useState(false);
+  const [channel, setChannel] = useState(null);
+
+  useEffect(() => {
+    const setupClient = async () => {
+      try {
+        await client.connectUser(
+          {
+            id: "akash-srivastava",
+            name: "Akash Srivastava",
+          },
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYWthc2gtc3JpdmFzdGF2YSJ9.xV8nMDDDDCW2EnjdJmkAEDhxUMwXV4CO3_VF8ghODj4"
+        );
+
+        const channel = await client.channel("livestream", "twitch-gaming", {
+          name: "Twitch Gaming",
+        });
+        setChannel(channel);
+
+        setClientReady(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setupClient();
+  }, []);
+
+  if (!clientReady) return null;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Chat client={client} theme="livestream dark">
+      <ChannelList filters={filters} sort={sort} />
+      <Channel channel={channel}>
+        <Window>
+          <ChannelHeader />
+          <MessageList />
+          <MessageInput />
+        </Window>
+        <Thread />
+      </Channel>
+    </Chat>
   );
-}
+};
 
 export default App;
