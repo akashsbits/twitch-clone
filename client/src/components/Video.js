fb62d50287c0dcbbdd18c8ba46756afe8bee5fa5
Akash Srivastava <sri.akash1992@gmail.com>
2021-04-24 17:08:31 +0530

    Displayed video using react-player

--- client/src/components/Video.js
+++ client/src/components/Video.js
@@ -1,5 +1,20 @@
+import ReactPlayer from "react-player";
+
 const Video = () => {
-  return <div className="video-container">Video</div>;
+  return (
+    <div className="video-container">
+      <ReactPlayer
+        className={"video-box"}
+        url={"video/sample.mp4"}
+        width={"100%"}
+        height={"100%"}
+        muted={true}
+        controls={false}
+        playing={true}
+        loop={true}
+      />
+    </div>
+  );
 };
 
 export default Video;
